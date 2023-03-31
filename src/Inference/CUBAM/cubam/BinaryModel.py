6bdeb214aaf5c6195f240555ca3bba81c8461562
jz81 <jingzhang.cs@gmail.com>
2023-03-31 11:10:17 +0800

    remove 7 inference algorithms

--- src/Inference/CUBAM/cubam/BinaryModel.py
+++
@@ -1,6 +0,0 @@
-from Model import Model
-from numpy import sign
-
-class BinaryModel(Model):
-    def __init__(self, filename=None, data=None):
-        Model.__init__(self, filename, data)
--- src/Inference/CUBAM/cubam/__init__.py
+++
@@ -1,2 +0,0 @@
-# make some submodules directly accessible
-
